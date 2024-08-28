e26add6d8933277437e33f6670658adb2c3fc6fb
crvernon <chrisrvernon@gmail.com>
2024-08-28 11:03:50 -0400

    add cloudpickle dependency

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ setup(
     install_requires=[
         'PyYAML>=5.4.1',
         'requests>=2.25.1',
-        'pyomo>=6.0.1'
+        'pyomo>=6.0.1',
+        'cloudpickle>=3.0.0',
     ],
     extras_require={
         'dev': [
@@ -45,7 +46,7 @@ setup(
             'sphinx-rtd-theme>=0.5.2',
             'twine>=3.4.1',
             'pytest>=6.2.4',
-            'pytest-cov>=2.12.1'
+            'pytest-cov>=2.12.1',
         ]
     }
 )
